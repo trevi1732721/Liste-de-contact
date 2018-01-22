a4a43f4a42ce0915a5390edc500d74aef6fb39fb
TreVi1732721 <vincent2000.tremblay@videotron.ca>
2018-01-22 15:47:30 -0500

    essaie 1

--- Occupation.java
+++ Occupation.java
@@ -2,4 +2,22 @@
  * Created by TreVi1732721 on 2018-01-22.
  */
 public class Occupation {
+    String poste;
+    Entreprise entreprise = new Entreprise();
+
+    public String getPoste() {
+        return poste;
+    }
+
+    public void setPoste(String poste) {
+        this.poste = poste;
+    }
+
+    public Entreprise getEntreprise() {
+        return entreprise;
+    }
+
+    public void setEntreprise(Entreprise entreprise) {
+        this.entreprise = entreprise;
+    }
 }
