b372e675dada2c08e80dbfbadc64ae44973d7df2
TreVi1732721 <vincent2000.tremblay@videotron.ca>
2018-01-25 13:19:47 -0500

    still have to do the main...

--- Entreprise.java
+++ Entreprise.java
@@ -2,4 +2,24 @@
  * Created by TreVi1732721 on 2018-01-22.
  */
 public class Entreprise {
+    String nom ;
+    Adresse adresseEntreprise = new Adresse() ;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Adresse getAdresseEntreprise() {
+        return adresseEntreprise;
+    }
+
+    public void setAdresseEntreprise(Adresse adresseEntreprise) {
+        this.adresseEntreprise = adresseEntreprise;
+    }
+
+
 }
--- Telephone.java
+++ Telephone.java
@@ -2,4 +2,21 @@
  * Created by TreVi1732721 on 2018-01-22.
  */
 public class Telephone {
+    String information, numero;
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
 }
